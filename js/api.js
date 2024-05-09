d95ed288c3dbfd368c595a6bd61b248864e8dd11
Mikkel TS <mikk75c4@stud.kea.dk>
2024-05-09 10:42:25 +0200

    added json server, and the importent api requests

---
+++ js/api.js
@@ -0,0 +1,56 @@
+
+const URL = "https://www.themealdb.com/api/json/v1/1/"
+
+export const searchMealByName = async (mealName) => {
+    if(!mealName){
+        return
+    }
+    try {
+        const response = await fetch(URL + "search.php?s=" + mealName)
+        return response.json()
+    } catch (error) {
+        throw new Error("Error fetching meal by name: " + error)
+    }
+}
+
+export const randomMeal = async () => {
+    try {
+        const response = await fetch(URL + "random.php")
+        return response.json()
+    } catch (error) {
+        throw new Error("Error fetching random meal: " + error)
+    }
+}
+
+export const searchMealById = async (mealId) => {
+    if(!mealId){
+        return
+    }
+    try {
+        const response = await fetch(URL + "lookup.php?i=" + mealId)
+        return response.json()
+    } catch (error) {
+        throw new Error("Error fetching meal by id: " + error)
+    }
+}
+
+export const listAllCategories = async () => {
+    try {
+        const response = await fetch(URL + "categories.php")
+        return response.json()
+    } catch (error) {
+        throw new Error("Error fetching list of categories: " + error)
+    }
+}
+
+export const filterByCategory =  async (category) => {
+    if (!category){
+        return
+    }
+    try {
+        const response = await fetch(URL + "filter.php?c=" + category)
+        return response.json()
+    } catch (error) {
+        throw new Error("Error filtering by category: " + error)
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,16 @@
+import { searchMealByName } from "../js/api.js";
+
+const searchForMealForm = document.getElementById("searchForMeal")
+if(searchForMealForm){
+    searchForMealForm.addEventListener("submit", async (e) => {
+        e.preventDefault()
+        console.log("I was pressed");
+        const searchInput = searchForMealForm.querySelector("#searchInput").value;
+        try {
+            const  response = await searchMealByName(searchInput)
+            console.log(response);
+        } catch (error){
+            console.log(error);
+        }
+    })
+}
