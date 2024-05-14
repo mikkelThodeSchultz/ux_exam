b8e92050324d81dbf9f41f7d590bc2d88dc490eb
Mikkel TS <mikk75c4@stud.kea.dk>
2024-05-14 09:30:22 +0200

    login, signup, API, and pages

---
+++ js/app.js
@@ -0,0 +1,64 @@
+import { searchMealByName } from "./mealApi.js";
+import { logIn, logout, signUp } from "./auth.js";
+
+const searchForMealForm = document.getElementById("searchForMeal")
+if(searchForMealForm){
+    searchForMealForm.addEventListener("submit", async (e) => {
+        e.preventDefault()
+        const searchInput = searchForMealForm.querySelector("#searchInput").value;
+        try {
+            const  response = await searchMealByName(searchInput)
+            console.log(response);
+        } catch (error){
+            console.log(error);
+        }
+    })
+}
+
+
+const setupSignupForm = document.getElementById("signUpForm")
+if(setupSignupForm){
+    setupSignupForm.addEventListener("submit", async (e) => {
+        e.preventDefault()
+        const formData = new FormData(setupSignupForm)
+        try{
+            const response = await signUp(formData)
+            if(response.status == 201){
+                window.location.href = "../views/login.html"
+                alert("User have been created succesfully")
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    })
+}
+
+const setupLoginForm = document.getElementById("logInForm")
+if(setupLoginForm){
+    setupLoginForm.addEventListener("submit", async (e) => {
+        e.preventDefault()
+        const formData = new FormData(setupLoginForm)
+        try{
+            const response = await logIn(formData)
+            if(!response){
+                alert("Wrong email or password")
+            }
+            else if(response.status == 200){
+                window.location.href = "../views/"
+                alert("User logged in succesfully")
+            } 
+        } catch (error) {
+            console.log(error);
+        }
+    })
+}
+
+const setupLogOut = document.getElementById("logOutBtn")
+if(setupLogOut){
+    setupLogOut.addEventListener("click", async (e) => {
+        e.preventDefault()
+        await logout()
+        window.location.href = "../views/"
+    })
+}
+
---
+++ js/auth.js
@@ -0,0 +1,84 @@
+
+const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+
+function validateEmail(email){
+    return EMAIL_REGEX.test(email);
+}
+function validatePassword(password){
+    return PASSWORD_REGEX.test(password);
+}
+
+const validateFormData = async (formData) => {
+    if(formData.get("user_email")){
+        const email = formData.get('user_email')
+        if(!validateEmail(email)){
+            alert("Must be a proper email.")
+            return false
+        }
+    }
+    if(formData.get("user_password")){
+        const password = formData.get('user_password')
+        if(!validatePassword(password)){
+            alert("The password must be between 8 and 20 characters, and contain lowercase and uppercase letters, numbers, and special characters.")
+            return false;
+        }
+        if(formData.get("user_password_confirm")){
+            const confirmPassword = formData.get("user_password_confirm")
+            if(password != confirmPassword){
+                alert("The password must match with confirm password.")
+                return false
+            }
+        }
+    }
+    return true
+} 
+
+
+export const signUp = async (formData) => {
+    if(!await validateFormData(formData)){
+        return;
+    }
+    const user_email = formData.get("user_email")
+    const user_password = formData.get("user_password")
+    const user = {
+        email: user_email,
+        password: user_password,
+        "favourites_id" : [
+        ]
+    }
+    try{
+        const response = await fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user)
+        })
+        return response
+    } catch (error) {
+        console.log(error);
+    }
+} 
+
+export const logIn = async (formData) => {
+    if(!await validateFormData(formData)){
+        return;
+    }
+    const user_email = formData.get("user_email")
+    const user_password = formData.get("user_password")
+    try {
+        const response = await fetch(`http://localhost:3000/users?email=${user_email}&password=${user_password}`);
+        const data = await response.json()
+        if (data){
+            sessionStorage.setItem('userEmail', data[0].email);
+        } 
+        return response
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const logout = async () => {
+    sessionStorage.clear();
+}
