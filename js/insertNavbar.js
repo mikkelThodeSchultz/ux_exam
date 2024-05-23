41844e2584900720f9aaf22b2b27abc9b49c99fa
jrw <jrw@magenta.dk>
2024-05-23 09:32:55 +0200

    removed separate navbar

--- js/insertNavbar.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('../_navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector('nav').innerHTML = data;
-        });
-});
