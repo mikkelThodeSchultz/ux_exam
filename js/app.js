b659ae28bb2e7d7700a6329c155b10deb7aaa6c1
Mikkel TS <mikk75c4@stud.kea.dk>
2024-05-23 10:13:46 +0200

    added favorites

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 import { handleLoginAndSignup } from './login.js';
 import { handleExplore } from './explore.js';
+import { setUpFavorites } from './favorites.js';
 
+const favoritesPageUrl = "http://127.0.0.1:5500/views/favorites.html";
 handleLoginAndSignup();
 handleExplore();
+
+const checkSite = () => {
+    const currentUrl = window.location.href;
+    if (currentUrl === favoritesPageUrl){
+      setUpFavorites()
+    }
+  };
+  
+  window.onload = checkSite();
