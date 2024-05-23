74ed9c7010e5afafbd8c244de05ee27b845a953c
Henry Navntoft <henrylnavntoft@gmail.com>
2024-05-23 14:35:05 +0200

    "..."

--- js/app.js
+++ js/app.js
@@ -1,16 +1,25 @@
-import { handleLoginAndSignup } from './login.js';
-import { handleExplore } from './explore.js';
-import { setUpFavorites } from './favorites.js';
+import { handleLoginAndSignup } from "./login.js";
+import { handleExplore } from "./explore.js";
+import { setUpFavorites } from "./favorites.js";
 
 const favoritesPageUrl = "http://127.0.0.1:5500/views/favorites.html";
 handleLoginAndSignup();
 handleExplore();
 
 const checkSite = () => {
-    const currentUrl = window.location.href;
-    if (currentUrl === favoritesPageUrl){
-      setUpFavorites()
-    }
-  };
-  
-  window.onload = checkSite();
+  const currentUrl = window.location.href;
+  if (currentUrl === favoritesPageUrl) {
+    setUpFavorites();
+  }
+};
+
+window.onload = checkSite();
+
+if (sessionStorage == null || sessionStorage.getItem("userEmail") == null) {
+  document.getElementById("logOutBtn").style.display = "none";
+  document.getElementById("loginBtn").style.display = "block";
+} else {
+  document.getElementById("logOutBtn").style.display = "block";
+  document.getElementById("loginBtn").style.display = "none";
+  document.getElementById("signUpBtn").style.display = "none";
+}
