1bc62ab4c6f8e686e4cdac2094c35e486509fce2
jrw <jrw@magenta.dk>
2024-05-28 15:40:57 +0200

    new foodcrd-css

--- js/foodCart.js
+++ js/foodCart.js
@@ -1,11 +1,8 @@
-import { addItemToLocalStorage,removeItemFromLocalStorage } from './auth.js';
-import { addMealToUser,removeMealFromUser } from './jsonApi.js';
+import { addItemToLocalStorage, removeItemFromLocalStorage } from './auth.js';
+import { addMealToUser, removeMealFromUser } from './jsonApi.js';
 
 export const createFoodCart = async (mealList) => {
-    const meal =
-    mealList && mealList.meals && mealList.meals[0]
-        ? mealList.meals[0]
-        : mealList;
+    const meal = mealList && mealList.meals && mealList.meals[0] ? mealList.meals[0] : mealList;
     const mealId = meal.idMeal;
     const mealName = meal.strMeal;
     const mealArea = meal.strArea;
@@ -13,14 +10,12 @@ export const createFoodCart = async (mealList) => {
     const mealIngredients = await getIngredients(meal);
     const mealInstructions = meal.strInstructions;
     const mealThumb = meal.strMealThumb;
-    //const mealTags = await getTags(meal.strTags);
-    //const mealYoutubeLink = meal.strYoutube;
 
     // Wrapper
     const cartSection = document.createElement('section');
     cartSection.classList.add('foodCart');
     cartSection.setAttribute('data-meal-id', mealId);
-  cartSection.tabIndex='0';
+    cartSection.tabIndex = '0';
 
     // Thumbnail Container
     const thumbContainer = document.createElement('div');
@@ -32,118 +27,125 @@ export const createFoodCart = async (mealList) => {
     img.alt = `Image of meal: ${mealName}`;
     img.classList.add('mealThumbnail');
 
-    // Buttons Container
-    const buttonsContainer = document.createElement('div');
-    buttonsContainer.classList.add('buttonsContainer');
+    // Add Button Container
+    const addBtnContainer = document.createElement('div');
+    addBtnContainer.classList.add('addBtnFeatured');
 
     if (isMealInFavorites(Number(mealId))) {
-    // Remove button
-        const removeButton = document.createElement('button');
-        removeButton.type = 'button';
-        removeButton.textContent = '-';
-        removeButton.classList.add('removeFromFavoritesButton');
-        removeButton.setAttribute('aria-label', 'Remove from favorites');
+        // Remove button
+        const removeButton = document.createElement('img');
+        removeButton.setAttribute('role', 'button');
+        removeButton.setAttribute('tabindex', '0');
+        removeButton.setAttribute('aria-label', 'Remove from favorites button');
+        removeButton.setAttribute('src', ' '); // tilføj billede
+        removeButton.classList.add('removeFromFavoritesWide');
         removeButton.addEventListener('click', async (e) => {
             e.stopPropagation();
             e.preventDefault();
             await removeFavorite(mealId, removeButton, e);
         });
-        buttonsContainer.appendChild(removeButton);
+        removeButton.addEventListener('keydown', async (e) => {
+            if (e.key === 'Enter' || e.key === ' ') {
+                e.stopPropagation();
+                e.preventDefault();
+                await removeFavorite(mealId, removeButton, e);
+            }
+        });
+        addBtnContainer.appendChild(removeButton);
     } else {
-    // Add button
-        const addButton = document.createElement('button');
-        addButton.type = 'button';
-        addButton.textContent = '+';
-        addButton.classList.add('addToFavoritesButton');
-        addButton.setAttribute('aria-label', 'Add to favorites');
+        // Add button
+        const addButton = document.createElement('img');
+        addButton.setAttribute('role', 'button');
+        addButton.setAttribute('tabindex', '0');
+        addButton.setAttribute('aria-label', 'Add to favorites button');
+        addButton.setAttribute('src', '../images/addBtn.svg');
+        addButton.classList.add('addToFavoritesWide');
         addButton.addEventListener('click', async (e) => {
             e.stopPropagation();
             e.preventDefault();
             await favoriteMeal(mealId, addButton, e);
         });
-        buttonsContainer.appendChild(addButton);
+        addButton.addEventListener('keydown', async (e) => {
+            if (e.key === 'Enter' || e.key === ' ') {
+                e.stopPropagation();
+                e.preventDefault();
+                await favoriteMeal(mealId, addButton, e);
+            }
+        });
+        addBtnContainer.appendChild(addButton);
     }
 
     thumbContainer.appendChild(img);
- 
+    thumbContainer.appendChild(addBtnContainer);
 
-  // Title Container
-  const titleContainer = document.createElement("div");
-  titleContainer.classList.add("title_container");
+    // Title Container
+    const titleContainer = document.createElement('div');
+    titleContainer.classList.add('title_container');
 
-  // Meal name
-  const mealNameHeading = document.createElement("h3");
-  mealNameHeading.classList.add("foodCart__title");
-  mealNameHeading.textContent = mealName;
+    // Meal name
+    const mealNameHeading = document.createElement('h3');
+    mealNameHeading.classList.add('foodCart__title');
+    mealNameHeading.textContent = mealName;
 
-  // Category and Area
-  const categoryAreaArticle = document.createElement("div");
-  categoryAreaArticle.classList.add("categoryArea");
+    // Category and Area
+    const categoryAreaArticle = document.createElement('div');
+    categoryAreaArticle.classList.add('categoryArea');
 
-  // Category
-  const categoryDiv = document.createElement("div");
-  categoryDiv.textContent = mealCategory;
+    // Category
+    const categoryDiv = document.createElement('div');
+    categoryDiv.textContent = mealCategory;
 
-  // Area
-  const areaDiv = document.createElement("div");
-  areaDiv.textContent = mealArea;
+    // Area
+    const areaDiv = document.createElement('div');
+    areaDiv.textContent = mealArea;
 
-  categoryAreaArticle.appendChild(categoryDiv);
-  categoryAreaArticle.appendChild(areaDiv);
+    categoryAreaArticle.appendChild(categoryDiv);
+    categoryAreaArticle.appendChild(areaDiv);
 
-  titleContainer.appendChild(mealNameHeading);
-  titleContainer.appendChild(categoryAreaArticle);
+    titleContainer.appendChild(mealNameHeading);
+    titleContainer.appendChild(categoryAreaArticle);
 
-  // Instructions
-  const instructionsParagraph = document.createElement("p");
-  instructionsParagraph.classList.add("foodCart__description");
-  instructionsParagraph.textContent = await shortenInstructions(
-    mealInstructions
-  );
+    // Instructions
+    const instructionsParagraph = document.createElement('p');
+    instructionsParagraph.classList.add('foodCart_description');
+    instructionsParagraph.textContent = await shortenInstructions(mealInstructions);
 
-  // Button Wrapper
-  const buttonWrapper = document.createElement("div");
-  buttonWrapper.classList.add("button_wrapper");
+    // Button Wrapper
+    const buttonWrapper = document.createElement('div');
+    buttonWrapper.classList.add('button_wrapper');
 
-  // Favorite button (Duplicate for the footer)
-  const footerButton = document.createElement("button");
-  footerButton.type = "button";
-  footerButton.textContent = "Add to favorites";
-  footerButton.classList.add("addToFavorites", "button");
-  footerButton.addEventListener("click", async (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-    favoriteMeal(mealId, footerButton, e);
-  });
+    // Favorite button (Duplicate for the footer)
+    const footerButton = document.createElement('button');
+    footerButton.type = 'button';
+    footerButton.textContent = 'Add to favorites';
+    footerButton.classList.add('addToFavorites', 'button');
+    footerButton.addEventListener('click', async (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        await favoriteMeal(mealId, footerButton, e);
+    });
 
-  buttonWrapper.appendChild(footerButton);
+    buttonWrapper.appendChild(footerButton);
 
-  cartSection.addEventListener("click", () =>
-    openModal(mealName, mealInstructions, mealIngredients, mealThumb)
-  );
-  cartSection.addEventListener("keydown", (e) => {
-    if (e.key === 'Enter') {
-        openModal(mealName, mealInstructions, mealIngredients, mealThumb);
-    }
-  });
+    cartSection.addEventListener('click', () =>
+        openModal(mealId, mealName, mealInstructions, mealIngredients, mealThumb)
+    );
 
-  cartSection.appendChild(thumbContainer);
-  cartSection.appendChild(titleContainer);
-  cartSection.appendChild(instructionsParagraph);
-  cartSection.appendChild(buttonWrapper);
+    cartSection.appendChild(thumbContainer);
+    cartSection.appendChild(titleContainer);
+    cartSection.appendChild(instructionsParagraph);
+    cartSection.appendChild(buttonWrapper);
 
-  return cartSection;
+    return cartSection;
 };
-
 
 const shortenInstructions = async (instructions) => {
-  if (instructions.length > 200) {
-    return instructions.substring(0, 200) + "...";
-  } else {
-    return instructions;
-  }
+    if (instructions.length > 50) {
+        return instructions.substring(0, 50) + '...';
+    } else {
+        return instructions;
+    }
 };
-
 
 const getIngredients = async (meal) => {
     const ingredients = [];
@@ -160,100 +162,140 @@ const getIngredients = async (meal) => {
     return ingredients;
 };
 
-// const getTags = async (tags) => {
-//     if (!tags) {
-//         return '';
-//     }
-//     const tagsArrays = tags.split(',');
-//     return tagsArrays.map((tag) => tag.trim());
-// };
-
 const favoriteMeal = async (mealId, addButton, e) => {
-  e.stopPropagation();
-  e.preventDefault();
-  const userEmail = sessionStorage.getItem("userEmail");
-  if (!userEmail) {
-    location.href = "../views/login.html";
-  }
-  const response = await addMealToUser(userEmail, mealId);
-  if (response.status === 200) {
-    addButton.textContent = "✓";
-    await addItemToLocalStorage(mealId, "favoritesIdList");
-  }
+    e.stopPropagation();
+    e.preventDefault();
+    const userEmail = sessionStorage.getItem('userEmail');
+    if (!userEmail) {
+        location.href = '../views/login.html';
+    }
+    const response = await addMealToUser(userEmail, mealId);
+    if (response.status === 200) {
+        addButton.textContent = '✓';
+        await addItemToLocalStorage(mealId, 'favoritesIdList');
+    }
 };
 
+const openModal = (mealId, mealName, instructions, ingredients, mealThumb) => {
+    const modal = document.getElementById('modal');
+    const closeBtn = document.querySelector('dialog#modal span.close');
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
 
-// TODO Add the rest of the data to the modal content
-const openModal = (mealName, instructions, ingredients, mealThumb) => {
-  const modal = document.querySelector("dialog#modal");
+    modal.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            modal.style.display = 'none';
+        }
+    });
 
-  const closeBtn = modal.querySelector("span.close");
-  
-  modal.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-      modal.style.display = "none";
+    const modalThumbnail = modal.querySelector('img');
+    const modalMealName = document.getElementById('modalMealName');
+    const modalInstructions = document.getElementById('modalInstructions');
+    const modalIngredients = document.getElementById('modalIngredients');
+    const modalButtonsContainer = document.getElementById('modalButtonsContainer');
+
+    modalThumbnail.src = mealThumb;
+    modalThumbnail.alt = `Meal thumbnail of: ${mealName}`;
+    modalMealName.textContent = mealName;
+    modalIngredients.innerHTML = '';
+    for (const ingredient of ingredients) {
+        const elem = document.createElement('li');
+        elem.textContent = ingredient.measure + ' ' + ingredient.ingredient;
+        modalIngredients.appendChild(elem);
     }
-  })
 
-  closeBtn.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
+    modalInstructions.textContent = instructions;
 
-  closeBtn.addEventListener("keydown", (e) => {
-    if (e.key === 'Enter') {
-      modal.style.display = "none";
+    modalButtonsContainer.innerHTML = '';
+
+    if (isMealInFavorites(Number(mealId))) {
+        // Remove button
+        const removeButton = document.createElement('img');
+        removeButton.setAttribute('role', 'button');
+        removeButton.setAttribute('tabindex', '0');
+        removeButton.setAttribute('aria-label', 'Remove from favorites button');
+        removeButton.setAttribute('src', ' '); // tilføj billede
+        removeButton.classList.add('removeFromFavoritesWide');
+        removeButton.addEventListener('click', async (e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            await removeFavorite(mealId, removeButton, e);
+        });
+        removeButton.addEventListener('keydown', async (e) => {
+            if (e.key === 'Enter' || e.key === ' ') {
+                e.stopPropagation();
+                e.preventDefault();
+                await removeFavorite(mealId, removeButton, e);
+            }
+        });
+        modalButtonsContainer.appendChild(removeButton);
+    } else {
+        // Add button
+        const addButton = document.createElement('img');
+        addButton.setAttribute('role', 'button');
+        addButton.setAttribute('tabindex', '0');
+        addButton.setAttribute('aria-label', 'Add to favorites button');
+        addButton.setAttribute('src', '../images/addBtn.svg');
+        addButton.classList.add('addToFavoritesWide');
+        addButton.addEventListener('click', async (e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            await favoriteMeal(mealId, addButton, e);
+        });
+        addButton.addEventListener('keydown', async (e) => {
+            if (e.key === 'Enter' || e.key === ' ') {
+                e.stopPropagation();
+                e.preventDefault();
+                await favoriteMeal(mealId, addButton, e);
+            }
+        });
+        modalButtonsContainer.appendChild(addButton);
     }
-  });
-  
 
-  closeBtn.addEventListener("keydown", (e) => {
-    if (e.key === 'Enter') {
-      modal.style.display = "none";
-    }
-  });
-  
-
-  const modalThumbnail = modal.querySelector("img");
-  const modalMealName = document.getElementById("modalMealName");
-  const modalInstructions = document.getElementById("modalInstructions");
-  const modalIngredients = document.getElementById("modalIngredience");
-
-  modalThumbnail.src = mealThumb;
-  modalThumbnail.alt = `Meal thumbnail of: ${mealName}`;
-  modalMealName.textContent = mealName;
-  modalIngredients.innerHTML = "";
-  for (const ingredient of ingredients) {
-    const elem = document.createElement("li");
-    elem.textContent = ingredient.measure + " " + ingredient.ingredient;
-    modalIngredients.appendChild(elem);
-  }
-
-  modalInstructions.textContent = instructions;
-  
-  modal.style.display = "flex";
-  modal.focus()
-  trapFocusInModal(modal)
+    modal.style.display = 'flex';
+    
 };
 
-//TODO Maybe this should be moved to another file
+const isMealInFavorites = (mealId) => {
+    const favorites = JSON.parse(localStorage.getItem('favoritesIdList')) || [];
+    return favorites.includes(mealId);
+};
+
+const removeFavorite = async (mealId, removeButton, e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    const userEmail = sessionStorage.getItem('userEmail');
+    if (!userEmail) {
+        location.href = '../views/login.html';
+    }
+    await removeItemFromLocalStorage(mealId, 'favoritesIdList');
+    const response = await removeMealFromUser(userEmail, mealId);
+    if (response.status === 200) {
+        removeButton.textContent = '✗';
+    } else {
+        console.log(response, mealId);
+    }
+};
+
 export const trapFocusInModal = (modal) => {
-  const focusableElements = modal.querySelectorAll('li, span[tabindex="0"]');  
-  const firstFocusableElement = focusableElements[0];
-  const lastFocusableElement = focusableElements[focusableElements.length - 1];
+    const focusableElements = modal.querySelectorAll('li, span[tabindex="0"]');  
+    const firstFocusableElement = focusableElements[0];
+    const lastFocusableElement = focusableElements[focusableElements.length - 1];
 
-  modal.addEventListener("keydown", (e) => {
-    if (e.key === "Tab") {
-      if (e.shiftKey) {
-        if (document.activeElement === firstFocusableElement) {
-          e.preventDefault();
-          lastFocusableElement.focus();
+    modal.addEventListener('keydown', (e) => {
+        if (e.key === 'Tab') {
+            if (e.shiftKey) {
+                if (document.activeElement === firstFocusableElement) {
+                    e.preventDefault();
+                    lastFocusableElement.focus();
+                }
+            } else {
+                if (document.activeElement === lastFocusableElement) {
+                    e.preventDefault();
+                    firstFocusableElement.focus();
+                }
+            }
         }
-      } else {
-        if (document.activeElement === lastFocusableElement) {
-          e.preventDefault();
-          firstFocusableElement.focus();
-        }
-      }
-    }
-  });
+    });
 };
